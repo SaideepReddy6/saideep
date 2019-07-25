2ea36a8358e653dd833586a6c7a265db66e0072b
SaideepReddy6 <52469511+SaideepReddy6@users.noreply.github.com>
2019-07-25 13:15:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,9 @@
-import os
+from flask import Flask
+app = Flask(__name__)
 
-from flask import  Flask,flash, request, redirect, render_template
-from werkzeug.utils import secure_filename
+@app.route("/")
+    def hello():
+        return "Hello World!"
 
-
-import pandas as pd
-
-app=Flask(__name__)
-
-	
-@app.route('/')
-def upload_form():
-	return render_template('upload.html')
-
-
-@app.route('/handleUpload', methods=['POST'])
-def upload_file():
-		file = request.files['file']
-		filename = secure_filename(file.filename)
-		file.save(os.path.join("/", filename))
-		return render_template('complete.html')
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
